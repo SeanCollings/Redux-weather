6697e6f1525a5f0e9d0a721c21120896c8752fa8
collingss <nightharrier@gmail.com>
2017-12-06 13:20:35 +0200

    first commmit update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-// Create a new component. This component should produce some HTML
-const App = function() {
-    return <div>Hi!</div>;
-}
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
